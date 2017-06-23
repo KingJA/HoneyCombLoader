ac0b48cc4b67b8a8d440e373903770d4592b9271
KingJA <kingjavip@gmail.com>
2017-06-23 16:57:13 +0800

    add attrs

--- honeycomb/src/main/java/com/kingja/honeycomb/HoneyCombLoader.java
+++ honeycomb/src/main/java/com/kingja/honeycomb/HoneyCombLoader.java
@@ -6,6 +6,7 @@ import android.animation.AnimatorSet;
 import android.animation.TypeEvaluator;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
@@ -17,6 +18,8 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 
+import java.util.concurrent.BlockingQueue;
+
 /**
  * Description:TODO
  * Create Time:2017/6/22 14:14
@@ -24,6 +27,11 @@ import android.view.animation.AccelerateInterpolator;
  * Email:kingjavip@gmail.com
  */
 public class HoneyCombLoader extends View {
+    private static final int DEFAULT_COLOR = 0xffCA5313;
+    private static final int DEFAULT_DURATION = 500;
+    private static final int DEFAULT_STYLE = 0;
+    private static final float DEFAULT_ALPHA = 0.2f;
+    private static final boolean DEFAULT_HIDE_BALL = false;
     private String TAG = "HoneyCombLoader";
     private float mViewSize;
     private boolean isFirst = true;
@@ -32,13 +40,18 @@ public class HoneyCombLoader extends View {
     private Paint mMoveBallPaint;
     private float mHoneyCombRadius;
     private float mPaintWidth;
-    private int color = 0xffCA5313;
+    private int mHoneyCombColor;
+    private int mHoneyBallColor;
+    private int mDuration;
+    private int mStyle;
+    private boolean mHideBall;
     private float mMoveBallRadius;
     private float mCurrentMoveBallX;
     private float mCurrentMOveBallY;
     private int mCurrentHoneyCombIndex;
     private float mFadeInAlpha = 255f;
     private float mFadeOutAlpha = 255f;
+    private float mHoneyCombAlpha;
     private AnimatorSet animatorSet;
 
 
@@ -52,22 +65,30 @@ public class HoneyCombLoader extends View {
 
     public HoneyCombLoader(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initAttrs();
+        initAttrs(attrs);
 
     }
 
-    private void initAttrs() {
-
+    private void initAttrs(AttributeSet attrs) {
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.HoneyCombLoader);
+        mHoneyCombColor = typedArray.getColor(R.styleable.HoneyCombLoader_honey_color, DEFAULT_COLOR);
+        mHoneyBallColor = typedArray.getColor(R.styleable.HoneyCombLoader_honey_ballColor, DEFAULT_COLOR);
+        mDuration = typedArray.getInt(R.styleable.HoneyCombLoader_honey_duration, DEFAULT_DURATION);
+        mStyle = typedArray.getInt(R.styleable.HoneyCombLoader_honey_style, DEFAULT_STYLE);
+        mHoneyCombAlpha = typedArray.getFloat(R.styleable.HoneyCombLoader_honey_alpha, DEFAULT_ALPHA);
+        mHideBall = typedArray.getBoolean(R.styleable.HoneyCombLoader_honey_hideBall, DEFAULT_HIDE_BALL);
+        typedArray.recycle();
     }
 
     private void initHoneyCombLoader() {
         mHoneyCombPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mHoneyCombPaint.setColor(color);
-        mHoneyCombPaint.setStyle(Paint.Style.STROKE);
+        mHoneyCombPaint.setStrokeCap(Paint.Cap.ROUND);
+        mHoneyCombPaint.setColor(mHoneyCombColor);
+        mHoneyCombPaint.setStyle(mStyle == 0 ? Paint.Style.STROKE : Paint.Style.FILL_AND_STROKE);
         mHoneyCombPaint.setStrokeWidth(mPaintWidth);
 
         mMoveBallPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mMoveBallPaint.setColor(color);
+        mMoveBallPaint.setColor(mHoneyBallColor);
         mMoveBallPaint.setStyle(Paint.Style.FILL);
     }
 
@@ -83,24 +104,23 @@ public class HoneyCombLoader extends View {
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         mPaintWidth = (int) (0.02f * mViewSize);
-        float marginX = 0.5f*mPaintWidth;
-        float marginY = (float) ((1.5f*marginX)/ Math.cos(30 * Math.PI / 180));
-        mHoneyCombRadius = (float) ((mViewSize - 2f * marginX -marginY) / 4f / Math.cos(30 * Math.PI / 180));
+        float marginX = 0.5f * mPaintWidth;
+        float marginY = (float) ((1.5f * marginX) / Math.cos(30 * Math.PI / 180));
+        mHoneyCombRadius = (float) ((mViewSize - 2f * marginX - marginY) / 4f / Math.cos(30 * Math.PI / 180));
         mMoveBallRadius = 0.1f * mHoneyCombRadius;
-        float mOffsetXFromAnother = (float) (Math.cos(30 * Math.PI / 180) * mHoneyCombRadius );
-        float mOffsetYFromAnother = 1.5f * mHoneyCombRadius ;
+        float mOffsetXFromAnother = (float) (Math.cos(30 * Math.PI / 180) * mHoneyCombRadius);
+        float mOffsetYFromAnother = 1.5f * mHoneyCombRadius;
         pointFs[0] = new PointF(0.5f * mViewSize, mHoneyCombRadius);
         pointFs[1] = new PointF(0.5f * mViewSize - mOffsetXFromAnother - marginX, mHoneyCombRadius +
-                mOffsetYFromAnother+ marginY);
+                mOffsetYFromAnother + marginY);
         pointFs[2] = new PointF(0.5f * mViewSize + mOffsetXFromAnother + marginX, mHoneyCombRadius +
-                mOffsetYFromAnother+ marginY);
+                mOffsetYFromAnother + marginY);
         initHoneyCombLoader();
     }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
-
         for (int i = 0; i < pointFs.length; i++) {
             if ((mCurrentHoneyCombIndex) % pointFs.length == i) {
                 mHoneyCombPaint.setAlpha((int) mFadeInAlpha);
@@ -113,17 +133,19 @@ public class HoneyCombLoader extends View {
         }
 
         if (isFirst) {
-            Log.e(TAG, "onDraw: ");
             startAnimator();
             isFirst = false;
         } else {
-            animateCircle(mCurrentMoveBallX, mCurrentMOveBallY, canvas);
+            if (!mHideBall) {
+                animateCircle(mCurrentMoveBallX, mCurrentMOveBallY, canvas);
+            }
+
         }
     }
 
 
     private void startAnimator() {
-        ValueAnimator fadeInAlphaAnimator = ValueAnimator.ofFloat(255 * 0.3f, 255f);
+        ValueAnimator fadeInAlphaAnimator = ValueAnimator.ofFloat(255 * mHoneyCombAlpha, 255f);
         fadeInAlphaAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
@@ -131,7 +153,7 @@ public class HoneyCombLoader extends View {
             }
         });
 
-        ValueAnimator fadeOutAlphaAnimator = ValueAnimator.ofFloat(255f, 255 * 0.3f);
+        ValueAnimator fadeOutAlphaAnimator = ValueAnimator.ofFloat(255f, 255 * mHoneyCombAlpha);
         fadeOutAlphaAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
@@ -160,7 +182,7 @@ public class HoneyCombLoader extends View {
         });
         animatorSet = new AnimatorSet();
         animatorSet.playTogether(fadeInAlphaAnimator, fadeOutAlphaAnimator, pointAnimator);
-        animatorSet.setDuration(500);
+        animatorSet.setDuration(mDuration);
         animatorSet.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
@@ -171,7 +193,7 @@ public class HoneyCombLoader extends View {
             @Override
             public void onAnimationCancel(Animator animation) {
                 super.onAnimationCancel(animation);
-                Log.e(TAG, "onAnimationCancel: " );
+                Log.e(TAG, "onAnimationCancel: ");
             }
         });
         animatorSet.start();
